e5d066e80a016243542be1abd378176a6c8ddfba
DIAZ RODRIGUEZ <alu3922@exthost2.(none)>
2013-10-18 12:41:27 +0100

    Practica Final

--- racional.rb
+++ racional.rb
@@ -3,4 +3,62 @@
 require "./gcd.rb"
 
 class Fraccion
+   attr_reader :numerador, :denominador
+   
+   def initialize(numerador,denominador)
+      @numerador = numerador
+      @denominador = denominador
+   end
+   def ==(other)
+      return @numerador == other.numerador && @denominador == other.denominador if other.instance_of? Fraccion
+   false
+   end 
+   def to_s
+      "#{@numerador}/#{@denominador}"
+   end
+
+   def suma(other)
+      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
+      a = Fraccion.new((mcm/@denominador) * @numerador, mcm)
+      b = Fraccion.new((mcm/other.denominador) * other.numerador, mcm)
+      c = Fraccion.new(a.numerador + b.numerador, mcm)
+   end
+
+   def resta(other)
+      mcm = (@denominador * other.denominador)/gcd(@denominador, other.denominador)
+      a = Fraccion.new((mcm/@denominador) * @numerador, mcm)
+      b = Fraccion.new((mcm/other.denominador) * other.numerador, mcm)
+      c = Fraccion.new(a.numerador - b.numerador, mcm)
+   end
+
+   def multiplicacion(other)
+      a = Fraccion.new(@numerador * other.numerador, @denominador * other.denominador)
+      max = gcd(a.numerador, a.denominador)
+      Fraccion.new(a.numerador/max, a.denominador/max)
+
+   end
+
+   def division(other)
+      a = Fraccion.new(@numerador * other.denominador, @denominador * other.numerador)
+     max = gcd(a.numerador, a.denominador)
+     Fraccion.new(a.numerador/max, a.denominador/max)
+   end
+
+
 end
+
+#a = Fraccion.new(7,20)
+#b = Fraccion.new(13,15)
+
+#c = a.suma(b)
+#c.to_s
+
+#c = a.resta(b)
+#c.to_s
+
+#c = a.multiplicacion(b)
+#c.to_s
+
+#c = a.division(b)
+#c.to_s
+
--- tc_racional.rb
+++ tc_racional.rb
@@ -5,4 +5,18 @@ require "test/unit"
 
 class Test_Fraccion < Test::Unit::TestCase
 
+   def setup
+      @a = Fraccion.new(7,20)
+      @b = Fraccion.new(13,15)
+   end
+
+   def test_sumayresta
+      assert_equal(Fraccion.new(73,60), @a.suma(@b))
+      assert_equal(Fraccion.new(-31,60), @a.resta(@b))
+   end
+
+   def test_multydiv
+      assert_equal(Fraccion.new(91,300), @a.multiplicacion(@b))
+      assert_equal(Fraccion.new(21,52), @a.division(@b))
+   end
 end
